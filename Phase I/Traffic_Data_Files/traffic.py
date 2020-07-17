c9c1ef6491ddaf239be2718f1f382628727176a2
Tong Xu <tongxu@MacBook-Pro.hitronhub.home>
2020-07-17 10:47:08 -0600

    Correct loading of traffic data to database, create method to read data from database and sort data.

--- Phase I/Traffic_Data_Files/traffic.py
+++ Phase I/Traffic_Data_Files/traffic.py
@@ -1,33 +1,44 @@
-import json
 import pandas as pd
 from pymongo import MongoClient
 
 def csv_to_dict(filename):
     data = pd.read_csv(filename)
+    data.columns = [x.lower() for x in data.columns]
     return data.to_dict('records')
 
 def extract_2018_accidents(filename):
     data = pd.read_csv(filename)
     data = data[data['id'].str.startswith('2018')]
+    data.columns = [x.lower() for x in data.columns]
     return data.to_dict('records')
 
 def main():
 
     client = MongoClient()
-    database = client['CalgaryTraffic']
-    collection1 = database['Traffic2016']
-    collection1.insert_many(csv_to_dict('TrafficFlow2016_OpenData.csv'))
-    collection2 = database['Traffic2017']
-    collection2.insert_many(csv_to_dict('2017_Traffic_Volume_Flow.csv'))
-    collection3 = database['Traffic2018']
-    collection3.insert_many(csv_to_dict('Traffic_Volumes_for_2018.csv'))
-    collection4 = database['Accident2016']
-    collection4.insert_many(csv_to_dict('Traffic_Incidents_Archive_2016.csv'))
-    collection5 = database['Accident2017']
-    collection5.insert_many(csv_to_dict('Traffic_Incidents_Archive_2017.csv'))
-    accident_2018 = extract_2018_accidents('Traffic_Incidents.csv')
-    collection6 = database['Traffic2018']
-    collection6.insert_many(accident_2018)
+    # database1 = client['CalgaryTrafficVol2016']
+    # collection1 = database1['Vol2016']
+    # collection1.insert_many(csv_to_dict('TrafficFlow2016_OpenData.csv'))
+    #
+    # database2 = client['CalgaryTrafficVol2017']
+    # collection2 = database2['Vol2017']
+    # collection2.insert_many(csv_to_dict('2017_Traffic_Volume_Flow.csv'))
+    #
+    # database3 = client['CalgaryTrafficVol2018']
+    # collection3 = database3['Vol2018']
+    # collection3.insert_many(csv_to_dict('Traffic_Volumes_for_2018.csv'))
+    #
+    # database4 = client['CalgaryTrafficAccident2016']
+    # collection4 = database4['Accident2016']
+    # collection4.insert_many(csv_to_dict('Traffic_Incidents_Archive_2016.csv'))
+    #
+    # database5 = client['CalgaryTrafficAccident2017']
+    # collection5 = database5['Accident2017']
+    # collection5.insert_many(csv_to_dict('Traffic_Incidents_Archive_2017.csv'))
+    #
+    # database6 = client['CalgaryTrafficAccident2018']
+    # accident_2018 = extract_2018_accidents('Traffic_Incidents.csv')
+    # collection6 = database6['Accident2018']
+    # collection6.insert_many(accident_2018)
 
 if __name__ == '__main__':
     main()
