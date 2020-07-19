7596275d7bf7b62e7c09fe8398da7056df1e2a43
Tong Xu <tongxu@MacBook-Pro.hitronhub.home>
2020-07-18 18:37:24 -0600

    Generalize read_2016 method.

--- Phase I/GUI Files/Project1_GUI.py
+++ Phase I/GUI Files/Project1_GUI.py
@@ -3,7 +3,6 @@ from Map import *
 from tkinter import *
 from tkinter import ttk
 import pandas as pd
-from pymongo import MongoClient
 
 
 class CalgaryTrafficGUI:
@@ -22,10 +21,10 @@ class CalgaryTrafficGUI:
         self.combobox2 = ttk.Combobox(left, width=15)
         self.combobox2['value'] = ("2016", "2017", "2018")
 
-        btn1 = Button(left, text="Read", height=3, width=20, command=read_2016)
+        btn1 = Button(left, text="Read", height=3, width=20, command=CalgaryTrafficGUI.read())
         btn2 = Button(left, text="Sort", height=3, width=20)
         btn3 = Button(left, text="Analysis", height=3, width=20)
-        btn4 = Button(left, text="Map", height=3, width=20, command=open_map)
+        btn4 = Button(left, text="Map", height=3, width=20, command=CalgaryTrafficGUI.open_map())
 
         label3 = ttk.Label(left, text="status:")
 
@@ -50,24 +49,13 @@ class CalgaryTrafficGUI:
 
         root.mainloop()
 
+    def read(self):
+        type_ = self.combobox1.get()
+        year = self.combobox2.get()
 
-    def read_data(type_, year):
-        """
-        Return traffic record for the specified type and year
-        :param type_: a string of either 'Vol' for traffic volume or 'Accident' for traffic accidents
-        :param year: a string for year '2016', '2017' or '2018'
-        :return: traffic records stored in a dataframe
-        """
-        client = MongoClient()
-        db = client['CalgaryTraffic' + type_ + year]
-        df = pd.DataFrame(list(db[type_ + year].find())).drop(columns = '_id')
-        return df
+        if type is not "" and year is not "":
+            df = read_data(type_, year)
 
-
-    def read_2016(self):
-        df_volume2016 = read_data('Vol', '2016')
-
-        df = pd.DataFrame(df_volume2016)
         cols = list(df.columns)
 
         tree = ttk.Treeview(self.right)
@@ -78,11 +66,9 @@ class CalgaryTrafficGUI:
             tree.heading(i, text=i, anchor='w')
 
         for index, row in df.iterrows():
-            tree.insert("",0,text=index,values=list(row))
-
+            tree.insert("", 0, text=index, values=list(row))
 
         tree.pack()
-
 
     def open_map(self):
         # Map Constructor Definition:
@@ -91,24 +77,23 @@ class CalgaryTrafficGUI:
         #  location = "17 Avenue at Richmond Road SW",
         #   map_base_coordinates = [51.044270, -114.062019]):
 
-        #Get the type and year user has selected
+        # Get the type and year user has selected
         map_type = self.combobox1.get()
         map_year = self.combobox2.get()
 
-        if(map_type is not "" and map_year is not ""):
+        if map_type is not "" and map_year is not "":
 
-            #TODO: Get the coordinate and location of the street with the most trafiic/accidents from the list specified by map_type and map_year
+            # TODO: Get the coordinate and location of the street with the most traffic/accidents from the list specified by map_type and map_year
 
-            map_object = Map(map_type, map_year) # Map(map_type,map_year,map_coordinate, map_location)
+            map_object = Map(map_type, map_year)  # Map(map_type,map_year,map_coordinate, map_location)
             # Update status on GUI
-            self.updateDepositLabel(""+map_object.get_File_Name()+"\ncreated")
+            self.update_deposit_label("" + map_object.get_File_Name() + "\ncreated")
 
         else:
-            self.updateDepositLabel("Error: Select Type/Year")
+            self.update_deposit_label("Error: Select Type/Year")
 
-
-    def updateDepositLabel(self, status) :
-        #status_txt.set("successfully loaded database")
+    def update_deposit_label(self, status):
+        # status_txt.set("successfully loaded database")
         self.status_txt.set(status)
         return self.status_txt
 
